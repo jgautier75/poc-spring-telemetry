e4c75141456c164689cccbdd20e6c08a6a8a1b9e
jga75 <jerome.gautier79@orange.fr>
2025-02-12 11:11:40 +0100

    Kafka / SchemaRegistry: Provide kafka & Schema Registry factory

--- adapter-kafka/src/test/java/com/acme/jga/kafka/consumer/KafkaSerializeDeserializeTest.java
+++ adapter-kafka/src/test/java/com/acme/jga/kafka/consumer/KafkaSerializeDeserializeTest.java
@@ -4,49 +4,34 @@ import com.acme.jga.utils.date.DateTimeUtils;
 import com.acme.users.mgt.events.protobuf.Event;
 import com.google.protobuf.DynamicMessage;
 import com.google.protobuf.InvalidProtocolBufferException;
-import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
-import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer;
-import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.CreateTopicsResult;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.Uuid;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.Description;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
-import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.kafka.ConfluentKafkaContainer;
 
-import java.net.UnknownHostException;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.awaitility.Awaitility.await;
-import static org.junit.Assert.*;
 
 @Slf4j
 class KafkaSerializeDeserializeTest {
@@ -56,25 +41,13 @@ class KafkaSerializeDeserializeTest {
     private static final int KAFKA_LISTENER_PORT = 19092;
 
     @Test
-    public void test() throws UnknownHostException, ExecutionException, InterruptedException {
+    public void Kafka_SchemaRegistry_Protobuf_Serialize_Deserialize() throws ExecutionException, InterruptedException {
 
         // Start kafka and schema registry containers
-        Network network = getNetwork();
-        ConfluentKafkaContainer confluentKafkaContainer = new ConfluentKafkaContainer("confluentinc/cp-kafka:" + CONFLUENT_PLATFORM_VERSION)
-                .withListener("kafka:" + KAFKA_LISTENER_PORT)
-                .withNetwork(network)
-                .withReuse(true);
-        GenericContainer<?> schemaRegistry = new GenericContainer<>("confluentinc/cp-schema-registry:" + CONFLUENT_PLATFORM_VERSION)
-                .withExposedPorts(SCHEMA_REGISTRY_PORT)
-                .dependsOn(confluentKafkaContainer)
-                .withNetworkAliases("schemaregistry")
-                .withNetwork(network)
-                .withEnv("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", "PLAINTEXT://kafka:" + KAFKA_LISTENER_PORT)
-                .withEnv("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:" + SCHEMA_REGISTRY_PORT)
-                .withEnv("SCHEMA_REGISTRY_HOST_NAME", "schemaregistry")
-                .withEnv("SCHEMA_REGISTRY_KAFKASTORE_SECURITY_PROTOCOL", "PLAINTEXT")
-                .waitingFor(Wait.forHttp("/subjects"))
-                .withStartupTimeout(Duration.of(120, ChronoUnit.SECONDS));
+        Network network = KafkaFactoryTest.createNetwork(KAFKA_NETWORK);
+        ConfluentKafkaContainer confluentKafkaContainer = KafkaFactoryTest.createConfluentKafkaContainer(CONFLUENT_PLATFORM_VERSION, KAFKA_LISTENER_PORT, network);
+        GenericContainer<?> schemaRegistry = KafkaFactoryTest.createSchemaRegistryContainer(CONFLUENT_PLATFORM_VERSION, SCHEMA_REGISTRY_PORT, network, confluentKafkaContainer, KAFKA_LISTENER_PORT);
+
         schemaRegistry.start();
         String schemaRegistryBaseUrl = String.format("http://%s:%d", schemaRegistry.getHost(), schemaRegistry.getMappedPort(SCHEMA_REGISTRY_PORT));
 
@@ -82,12 +55,12 @@ class KafkaSerializeDeserializeTest {
         String topicName = "audit_events";
         String consumerGroupId = "audit_consumer";
 
-        KafkaFuture<Uuid> uuidKafkaFuture = kafkaCreateTopic(confluentKafkaContainer.getBootstrapServers(), topicName, 1, (short) 1);
+        KafkaFuture<Uuid> uuidKafkaFuture = KafkaFactoryTest.kafkaCreateTopic(confluentKafkaContainer.getBootstrapServers(), topicName, 1, (short) 1);
         await().until(uuidKafkaFuture::isDone);
         log.info("Created topic [{}] with uuid [{}]", topicName, uuidKafkaFuture.get().toString());
 
         // Setup kafka template
-        KafkaTemplate<String, Event.AuditEventMessage> kafkaTemplate = kafkaTemplateAudit(confluentKafkaContainer.getBootstrapServers(), schemaRegistryBaseUrl);
+        KafkaTemplate<String, Event.AuditEventMessage> kafkaTemplate = KafkaFactoryTest.kafkaCreateTemplate(confluentKafkaContainer.getBootstrapServers(), schemaRegistryBaseUrl);
 
         // Create record and send
         Event.AuditEventMessage producerEvent = createAuditEvent();
@@ -95,107 +68,41 @@ class KafkaSerializeDeserializeTest {
         CompletableFuture<SendResult<String, Event.AuditEventMessage>> sendResult = kafkaTemplate.send(producerRecord);
         await().until(sendResult::isDone);
 
-        KafkaConsumer<String, DynamicMessage> consumer = new KafkaConsumer<>(consumerConfigs(confluentKafkaContainer.getBootstrapServers(), consumerGroupId, schemaRegistryBaseUrl));
-        consumer.subscribe(List.of(topicName));
-        AtomicBoolean protobufDeserializationError = new AtomicBoolean(false);
-        Event.AuditEventMessage auditEventMessage = null;
-
-        while (true) {
-            ConsumerRecords<String, DynamicMessage> consumerRecords = consumer.poll(Duration.ofSeconds(10));
-            for (ConsumerRecord<String, DynamicMessage> consumerRecord : consumerRecords) {
-                try {
-                    auditEventMessage = Event.AuditEventMessage.newBuilder().build().getParserForType().parseFrom(consumerRecord.value().toByteArray());
-                } catch (InvalidProtocolBufferException e) {
-                    protobufDeserializationError.set(true);
+        try (KafkaConsumer<String, DynamicMessage> consumer = KafkaFactoryTest.kafkaCreateConsumer(confluentKafkaContainer.getBootstrapServers(), consumerGroupId, schemaRegistryBaseUrl)) {
+            consumer.subscribe(List.of(topicName));
+            AtomicBoolean protobufDeserializationError = new AtomicBoolean(false);
+            Event.AuditEventMessage auditEventMessage = null;
+            while (true) {
+                ConsumerRecords<String, DynamicMessage> consumerRecords = consumer.poll(Duration.ofSeconds(10));
+                for (ConsumerRecord<String, DynamicMessage> consumerRecord : consumerRecords) {
+                    try {
+                        auditEventMessage = Event.AuditEventMessage.newBuilder().build().getParserForType().parseFrom(consumerRecord.value().toByteArray());
+                    } catch (InvalidProtocolBufferException e) {
+                        protobufDeserializationError.set(true);
+                    }
+                    break;
                 }
                 break;
             }
-            break;
-        }
-        assertFalse(protobufDeserializationError.get());
-        assertNotNull(auditEventMessage);
-        assertEquals("Object uid match", producerEvent.getObjectUid(), auditEventMessage.getObjectUid());
-    }
 
-    public ConsumerFactory<Integer, String> consumerFactory(String bootstrapServers, String clientId, String schemaRegistryBaseUrl) {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs(bootstrapServers, clientId, schemaRegistryBaseUrl));
-    }
+            Assertions.assertFalse(protobufDeserializationError.get());
+            Assertions.assertNotNull(auditEventMessage);
+            Assertions.assertEquals(producerEvent.getObjectUid(), auditEventMessage.getObjectUid(), "Object uid match");
+            listSchemaRegistry(schemaRegistryBaseUrl);
 
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, String>> kafkaListenerContainerFactory(String bootstrapServers, String clientId, String schemaRegistryBaseUrl) {
-        ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory(bootstrapServers, clientId, schemaRegistryBaseUrl));
-        factory.setConcurrency(3);
-        factory.getContainerProperties().setPollTimeout(3000);
-        return factory;
-    }
-
-    public Map<String, Object> consumerConfigs(String bootstrapServers, String clientId, String schemaRegistryBaseUrl) {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, clientId);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaProtobufDeserializer.class);
-        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryBaseUrl);
-        return props;
-    }
-
-    private KafkaTemplate<String, Event.AuditEventMessage> kafkaTemplateAudit(String bootstrapServers, String schemaRegistryBaseUrl) {
-        return new KafkaTemplate<>(producerFactory(bootstrapServers, schemaRegistryBaseUrl));
-    }
-
-    public ProducerFactory<String, Event.AuditEventMessage> producerFactory(String bootstrapServers, String schemaRegistryBaseUrl) {
-        return new DefaultKafkaProducerFactory<>(producerConfigs(bootstrapServers, schemaRegistryBaseUrl));
-    }
-
-    public Map<String, Object> producerConfigs(String bootstrapServers, String schemaRegistryBaseUrl) {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class);
-        props.put(AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS, true);
-        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryBaseUrl);
-        return props;
-    }
-
-    private KafkaFuture<Uuid> kafkaCreateTopic(String bootStrapServers, String topicName, int nbOfPartitions, short replicationFactor) {
-        Properties props = new Properties();
-        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
-        props.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, 1000);
-        props.put(AdminClientConfig.DEFAULT_API_TIMEOUT_MS_CONFIG, 1000);
-        try (AdminClient adminClient = AdminClient.create(props)) {
-            NewTopic topic = new NewTopic(topicName, nbOfPartitions, replicationFactor);
-            CreateTopicsResult createTopicsResult = adminClient.createTopics(List.of(topic));
-            return createTopicsResult.topicId(topicName);
         }
     }
 
-    private Network getNetwork() {
-        Network defaultDaprNetwork = new Network() {
-            @Override
-            public org.junit.runners.model.Statement apply(org.junit.runners.model.Statement base, Description description) {
-                return null;
-            }
-
-            @Override
-            public String getId() {
-                return KAFKA_NETWORK;
-            }
-
-            @Override
-            public void close() {
-            }
-        };
-        List<com.github.dockerjava.api.model.Network> networks = DockerClientFactory.instance().client().listNetworksCmd().withNameFilter(KAFKA_NETWORK).exec();
-        if (networks.isEmpty()) {
-            Network.builder()
-                    .createNetworkCmdModifier(cmd -> cmd.withName(KAFKA_NETWORK))
-                    .build().getId();
-            return defaultDaprNetwork;
-        } else {
-            return defaultDaprNetwork;
+    private void listSchemaRegistry(String schemaRegistryBaseUrl) {
+        CachedSchemaRegistryClient cachedSchemaRegistryClient = new CachedSchemaRegistryClient(schemaRegistryBaseUrl, 10);
+        try {
+            cachedSchemaRegistryClient.getAllSubjects().forEach(sub -> {
+                log.info("Subject [{}]", sub);
+            });
+        } catch (Exception e) {
+            // Silent catch
         }
+
     }
 
     private Event.AuditEventMessage createAuditEvent() {
